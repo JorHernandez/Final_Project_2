2fb90f542aa06f437dd6018793e277fe905c73fb
JorHernandez <jorgeh@broadvoice.com>
2018-06-19 07:46:06 -0700

    Prevent default
    Added default default check

--- Final_Project/JS/Scripts.js
+++ Final_Project/JS/Scripts.js
@@ -7,38 +7,38 @@
     //Slide district name to the header
 
 
-$("#btnResi").click
+$("#btnResi").click(function(e)
 {
 changeDisName("1234 Resi Street");
 console.log('Click');
 event.preventDefault();
-}
+})
 
-$("#btnInd").click
+$("#btnInd").click(function(e)
 {
     changeDisName("456 Industrial Way");
     console.log('Click2');
     event.preventDefault();
-    }
+    })
 
-$("#btnComm").click
+$("#btnComm").click(function(e)
 {
     changeDisName("789 Commercial Blvd");
     console.log('Click3');
     event.preventDefault();
-}
+})
     
-$("#btnMResi").click
+$("#btnMResi").click(function(e)
 {
     changeDisName("1011 Multiresi Pl");
     console.log('Click4');
     event.preventDefault();
-}
+})
 
 
 
 
-// changeDisName ('1234 Resi Street');
+
 
 function changeDisName (text) {
 $(".header-info h1").html(text);
