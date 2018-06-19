b23622c2e9c518012aaf16bb39221cc54ff61988
JorHernandez <jorgeh@broadvoice.com>
2018-06-18 18:54:28 -0700

    styling and such
    added more styling and JS functions

--- Final_Project/JS/Scripts.js
+++ Final_Project/JS/Scripts.js
@@ -5,3 +5,27 @@
 //When drop down is selected:
     //Hide the drop down menu
     //Slide district name to the header
+
+
+$("#btnResi").click(function(){
+console.log('Click');
+})
+
+$("#btnInd").click(function(){
+    console.log('Click2');
+    })
+
+$("#btnComm").click(function(){
+console.log('Click3');
+})
+    
+$("#btnMResi").click(function(){
+console.log('Click4');
+})
+
+
+
+
+function mainPage (){
+    $('#district-name')display='none';
+} 
