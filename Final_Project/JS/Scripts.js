157f861c8431b2f43efa3d1506d33b519063fa97
JorHernandez <jorgeh@broadvoice.com>
2018-06-18 17:34:01 -0700

    Boot strap
    added bootstrap
    added more divs and name
    revamped drop down for BS
    started writing out JS

---
+++ Final_Project/JS/Scripts.js
@@ -0,0 +1,7 @@
+//I: Select one of the options
+//O: The option will bring up images related to the address
+
+
+//When drop down is selected:
+    //Hide the drop down menu
+    //Slide district name to the header
