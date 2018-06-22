74e857157a5fd9c0bf6168f36af05ac29aa0cf4d
JorHernandez <jorgeh@broadvoice.com>
2018-06-21 21:05:05 -0700

    more
    more work on functions....or attempting to

--- Final_Project/JS/Scripts.js
+++ Final_Project/JS/Scripts.js
@@ -41,13 +41,38 @@ function changeDisName(text) {
     $(".header-info h1").html(text);
 }
 
-function displayResi () {
-    $(".map").html(<img src="../Images/Residential/ResiMap_View.PNG" />);
-}
+// function displayResi () {
+//     $(".map").html(<img src="../Images/Residential/ResiMap_View.PNG" />);
+// }
 
 
+// $(".map").click
+
+// (function displayResi (){
+//     $("#btnResi").toggleClass("../Images/Multi_Family/MFamBuilding_Info.PNG")  ; 
+//    })
+
+
+
+//    function displayResi (){
+// 	var image = document.getElementById('#map');
+// 	image src = ".BusiInfo";
+//}
+   
 // change district name 
 // display images
 // hide other imagess
 //e preventdefault
 //nice to have: BS modal pop up for each image. 
+
+
+$("#map").attr("src","../Images/Business/BusiMap.PNG");
+
+$('#map').on({
+    'click': function displayResi(){
+        $('#map').attr('src','second.jpg');
+    }
+});
+// Then you can change the src in jQuery by:
+
+// To attach this to a click event, you could write:
