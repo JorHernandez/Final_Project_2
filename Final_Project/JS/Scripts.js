c65ee272690244ad25c35aa3f2dfb2145c748c6e
JorHernandez <jorgeh@broadvoice.com>
2018-06-25 14:29:44 -0700

    Clean up
    Final clean up.
    
    Proof of concept is officialy done. This is considered the pre-alpha(?)

--- Final_Project/JS/Scripts.js
+++ Final_Project/JS/Scripts.js
@@ -27,92 +27,87 @@ $("#btnMResi").click(function (e) {
 })
 
 function changeDisName(text) {
-     $(".header-info h1").html(text);
- }
+    $(".header-info h1").html(text);
+}
 
-function changeResi(){
-    $('#map').attr('src', '../Images/Residential/ResiMap_View.PNG' );
-    $('#info').attr('src','../Images/Residential/ResiBuilding_Info.PNG');
-    $('#alpha').attr('src','../Images/Residential/ResiAlpha_Bravo.PNG');
-    $('#bravo').attr('src','../Images/Residential/ResiAlpha_Bravo.PNG');
-    $('#charlie').attr('src','../Images/Residential/ResiBravo_Charlie.PNG');
-    $('#delta').attr('src','../Images/Residential/ResiCharlie_Delta.PNG');
+function changeResi() {
+    $('#map').attr('src', '../Images/Residential/ResiMap_View.PNG');
+    $('#info').attr('src', '../Images/Residential/ResiBuilding_Info.PNG');
+    $('#alpha').attr('src', '../Images/Residential/ResiAlpha_Bravo.PNG');
+    $('#bravo').attr('src', '../Images/Residential/ResiAlpha_Bravo.PNG');
+    $('#charlie').attr('src', '../Images/Residential/ResiBravo_Charlie.PNG');
+    $('#delta').attr('src', '../Images/Residential/ResiCharlie_Delta.PNG');
 }
 
 function changeInd() {
-    $('#map').attr('src', '../Images/Industrial/IndMap_view.PNG' );
-    $('#info').attr('src','../Images/Industrial/IndBuilding_Info.PNG');
-    $('#alpha').attr('src','../Images/Industrial/IndAlpha_Bravo.PNG');
-    $('#bravo').attr('src','../Images/Industrial/IndBravo_Charlie.PNG');
-    $('#charlie').attr('src','../Images/Industrial/IndCharlie_Delta.PNG');
-    $('#delta').attr('src','../Images/Industrial/IndAlpha_Delta.PNG');
+    $('#map').attr('src', '../Images/Industrial/IndMap_view.PNG');
+    $('#info').attr('src', '../Images/Industrial/IndBuilding_Info.PNG');
+    $('#alpha').attr('src', '../Images/Industrial/IndAlpha_Bravo.PNG');
+    $('#bravo').attr('src', '../Images/Industrial/IndBravo_Charlie.PNG');
+    $('#charlie').attr('src', '../Images/Industrial/IndCharlie_Delta.PNG');
+    $('#delta').attr('src', '../Images/Industrial/IndAlpha_Delta.PNG');
 }
 
 
-function changeComm(){
-    $('#map').attr('src', '../Images/Business/BusiMap.PNG' );
-    $('#info').attr('src','../Images/Business/BusiBuilding_Info.PNG');
-    $('#alpha').attr('src','../Images/Business/BusiAlpha.PNG');
-    $('#bravo').attr('src','../Images/Business/BusiBravo.PNG');
-    $('#charlie').attr('src','../Images/Business/BusiCharlie.PNG');
-    $('#delta').attr('src','../Images/Business/BusiDelta.PNG');
+function changeComm() {
+    $('#map').attr('src', '../Images/Business/BusiMap.PNG');
+    $('#info').attr('src', '../Images/Business/BusiBuilding_Info.PNG');
+    $('#alpha').attr('src', '../Images/Business/BusiAlpha.PNG');
+    $('#bravo').attr('src', '../Images/Business/BusiBravo.PNG');
+    $('#charlie').attr('src', '../Images/Business/BusiCharlie.PNG');
+    $('#delta').attr('src', '../Images/Business/BusiDelta.PNG');
 }
 
 function changeMFam() {
-    $('#map').attr('src', '../Images/Multi_Family/MFamMap_View.PNG' );
-    $('#info').attr('src','../Images/Multi_Family/MFamBuilding_Info.PNG');
-    $('#alpha').attr('src','../Images/Multi_Family/MFamAlpha_Bravo.PNG');
-    $('#bravo').attr('src','../Images/Multi_Family/MFamBravo_Charlie.PNG');
-    $('#charlie').attr('src','../Images/Multi_Family/MFamCharlie_Delta.PNG');
-    $('#delta').attr('src','../Images/Multi_Family/MFamAlpha_Delta.PNG');
+    $('#map').attr('src', '../Images/Multi_Family/MFamMap_View.PNG');
+    $('#info').attr('src', '../Images/Multi_Family/MFamBuilding_Info.PNG');
+    $('#alpha').attr('src', '../Images/Multi_Family/MFamAlpha_Bravo.PNG');
+    $('#bravo').attr('src', '../Images/Multi_Family/MFamBravo_Charlie.PNG');
+    $('#charlie').attr('src', '../Images/Multi_Family/MFamCharlie_Delta.PNG');
+    $('#delta').attr('src', '../Images/Multi_Family/MFamAlpha_Delta.PNG');
 }
 
 
 
-$(function() {
-    $('.popMap').on('click', function() {
+$(function () {
+    $('.popMap').on('click', function () {
         $('.imagepreview').attr('src', $(this).find('img').attr('src'));
-        $('#imagemodal').modal('show');          
-    });		
+        $('#imagemodal').modal('show');
+    });
 });
 
-$(function() {
-    $('.popInfo').on('click', function() {
+$(function () {
+    $('.popInfo').on('click', function () {
         $('.imagepreview').attr('src', $(this).find('img').attr('src'));
-        $('#imagemodal').modal('show');         
-    });		
+        $('#imagemodal').modal('show');
+    });
 });
 
-$(function() {
-    $('.popAlpha').on('click', function() {
+$(function () {
+    $('.popAlpha').on('click', function () {
         $('.imagepreview').attr('src', $(this).find('img').attr('src'));
-        $('#imagemodal').modal('show');         
-    });		
+        $('#imagemodal').modal('show');
+    });
 });
 
-$(function() {
-    $('.popBravo').on('click', function() {
+$(function () {
+    $('.popBravo').on('click', function () {
         $('.imagepreview').attr('src', $(this).find('img').attr('src'));
-        $('#imagemodal').modal('show');          
-    });		
+        $('#imagemodal').modal('show');
+    });
 });
 
-$(function() {
-    $('.popCharlie').on('click', function() {
+$(function () {
+    $('.popCharlie').on('click', function () {
         $('.imagepreview').attr('src', $(this).find('img').attr('src'));
-        $('#imagemodal').modal('show'); 
-         
-    });		
+        $('#imagemodal').modal('show');
+
+    });
 });
 
-$(function() {
-    $('.popDelta').on('click', function() {
+$(function () {
+    $('.popDelta').on('click', function () {
         $('.imagepreview').attr('src', $(this).find('img').attr('src'));
-        $('#imagemodal').modal('show');           
-    });		
+        $('#imagemodal').modal('show');
+    });
 });
-
-
-
-
-
